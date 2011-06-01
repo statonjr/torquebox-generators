e7a14831a274ef2b5f950367e821d704bb816548
Larry Staton Jr <statonjr@gmail.com>
2011-05-31 23:04:23 -0400

    Tweak module name again.

--- lib/generators/torquebox/job/job_generator.rb
+++ lib/generators/torquebox/job/job_generator.rb
@@ -1,4 +1,4 @@
-module TorqueBox
+module Torquebox
   class JobGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
--- lib/generators/torquebox/service/service_generator.rb
+++ lib/generators/torquebox/service/service_generator.rb
@@ -1,4 +1,4 @@
-module TorqueBox
+module Torquebox
   class ServiceGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
 
